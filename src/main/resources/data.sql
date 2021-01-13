c17d5852bcf0af05dc8194744e1feb9fb473f5b7
Konrad Lesiak <kaskaan@gmail.com>
2021-01-13 20:11:02 +0000

    adding 'data.sql' file into 'resources' folder, and initialize some startup data. closes #13

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (description) values ('American');
+INSERT INTO category (description) values ('Italian');
+INSERT INTO category (description) values ('Mexican');
+INSERT INTO category (description) values ('Fast Food');
+
+INSERT INTO unit_of_measure (unit_name) values ('Fast Food');
+INSERT INTO unit_of_measure (unit_name) values ('Teaspoon');
+INSERT INTO unit_of_measure (unit_name) values ('Tablespoon');
+INSERT INTO unit_of_measure (unit_name) values ('Cup');
+INSERT INTO unit_of_measure (unit_name) values ('Pinch');
+INSERT INTO unit_of_measure (unit_name) values ('Ounce');
