85c9c979e06677a8b67bc0399bc4982b0b45032a
Konrad Lesiak <kaskaan@gmail.com>
2021-01-14 14:35:59 +0000

    adding RecipeService to RecipeController and updating index.html. closes #24

--- src/main/java/com/konradlesiak/controller/RecipeController.java
+++ src/main/java/com/konradlesiak/controller/RecipeController.java
@@ -1,13 +1,23 @@
 package com.konradlesiak.controller;
 
+import com.konradlesiak.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RecipeController {
 
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
