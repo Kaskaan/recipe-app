d9eea42adbb5ff0128e165f9a0071eecc89fa5f7
Konrad Lesiak <kaskaan@gmail.com>
2021-01-14 14:24:15 +0000

    making inserted data.sql values unique. closes #21

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,12 @@ INSERT INTO category (description) values ('Italian');
 INSERT INTO category (description) values ('Mexican');
 INSERT INTO category (description) values ('Fast Food');
 
-INSERT INTO unit_of_measure (unit_name) values ('Fast Food');
 INSERT INTO unit_of_measure (unit_name) values ('Teaspoon');
+INSERT INTO unit_of_measure (unit_name) values ('Each');
 INSERT INTO unit_of_measure (unit_name) values ('Tablespoon');
 INSERT INTO unit_of_measure (unit_name) values ('Cup');
 INSERT INTO unit_of_measure (unit_name) values ('Pinch');
 INSERT INTO unit_of_measure (unit_name) values ('Ounce');
+INSERT INTO unit_of_measure (unit_name) values ('Piece');
+INSERT INTO unit_of_measure (unit_name) values ('Dash');
+INSERT INTO unit_of_measure (unit_name) values ('Pint');
