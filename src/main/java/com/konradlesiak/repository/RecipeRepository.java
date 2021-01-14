421921aafefaf426e0e3420c57d4a3e4853d420c
Konrad Lesiak <kaskaan@gmail.com>
2021-01-14 14:19:41 +0000

    adding "@Repository" annotation (closes #17). Adding possibility to findUnitByName (closes #18). Adding possibility to find category by description (closes #19)

--- src/main/java/com/konradlesiak/repository/RecipeRepository.java
+++ src/main/java/com/konradlesiak/repository/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.konradlesiak.repository;
 
 import com.konradlesiak.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 }
