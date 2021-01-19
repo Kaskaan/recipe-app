4c48d8a86a7850fa54a59c58af009e79d0bc64f1
Konrad Lesiak <kaskaan@gmail.com>
2021-01-19 13:11:21 +0000

    adding Integration Test to UnitOfMeasure. closes #27

---
+++ src/test/java/com/konradlesiak/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,37 @@
+package com.konradlesiak.repository;
+
+import com.konradlesiak.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByUnitName() {
+        final Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUnitName("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getUnitName());
+    }
+
+    @Test
+    void findByUnitNameCup() throws Exception {
+        final Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUnitName("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getUnitName());
+    }
+}
